eaf47f398cb0d26bc26b587872279b14b139ac08
raj-rupesh <108718403+raj-rupesh@users.noreply.github.com>
2022-10-15 20:02:50 +0530

    repo setup

---
+++ components/Landing.js
@@ -0,0 +1,13 @@
+export default function Landing() {
+  return (
+    <div
+      className="flex flex-col justify-center"
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <div className="text-amber-400 text-center font-bold">
+        <h1 className="text-3xl">Under Construction!</h1>
+        <p className="text-2xl">Check back soon 😅</p>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import Landing from "../components/Landing";
+
+export default () => {
+  return <Landing />;
+};
