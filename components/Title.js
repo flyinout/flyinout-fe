9e38a486fe4b59b523258b52b66b9ab7654a42b4
zigmint <rupeshr053@gmail.com>
2022-12-14 05:47:59 +0530

    updates

--- components/Title.js
+++ components/Title.js
@@ -4,14 +4,14 @@ import { SectionHeading, SectionSubHeading } from "./Hero";
 export default function Title() {
   return (
     <div className="text-center flex flex-col items-center mt-8 text-white">
-      <button className="mb-4 btn btn-sm normal-case text-lg text-primary rounded-lg px-4 bg-yellow-500 hover:bg-yellow-600 duration-300">
+      <button className="mb-4 normal-case text-lg text-primary rounded-lg px-4 bg-yellow-500 hover:bg-yellow-600 duration-300">
         Beta Access
       </button>
       <SectionHeading>The Ultimate Modern Recruiting Platform</SectionHeading>
       <SectionSubHeading>
-        FlyinOut is not just your platform to find candiadtes we use state of
-        the art machine learning technogies to close down on your recruitment
-        needs.
+        Where talent takes flight your next hire is just a flight away helping
+        you soar to success with the right hire Bringing the best together,
+        Flyinout
       </SectionSubHeading>
       <Email></Email>
     </div>
