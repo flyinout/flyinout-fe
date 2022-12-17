751e5c0984183ee815da4783023a1e494b2d8b21
zigmint <rupeshr053@gmail.com>
2022-12-17 13:02:58 +0530

    add team section

---
+++ components/Features.js
@@ -0,0 +1,103 @@
+import { SectionHeading, SectionSubHeading } from "./Hero";
+
+export default function Features() {
+  return (
+    <>
+      <div className="text-center flex flex-col w-full items-center mt-10 mb-8 ">
+        <SectionHeading>Features</SectionHeading>
+        <SectionSubHeading>
+          Custom features based on your needs , we do everything you wish for
+          though!
+        </SectionSubHeading>
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:items-center md:justify-between w-full">
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:items-center md:justify-between w-full">
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+        <div
+          className="my-2 mx-4 p-8 md:mx-8 rounded-lg text-left max-w-md"
+          style={{ background: "#383440" }}
+        >
+          <div
+            className="w-8 h-8 rounded-lg"
+            style={{ background: "#948DA9" }}
+          ></div>
+          <h3 className="text-md text-primary font-bold "> Manage</h3>
+          <p className="text-secondary">
+            Manage fine grained access controls for your team. Set goals for
+            your team on a organization level.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/Landing.js
+++ components/Landing.js
@@ -7,6 +7,7 @@ import Automations from "./Automations";
 import Collaboration from "./Collaboration";
 import EarlyBird from "./EarlyBird";
 import Imports from "./Imports";
+import Features from "./Features";
 
 /* eslint-disable @next/next/no-img-element */
 export default function Landing() {
@@ -15,9 +16,10 @@ export default function Landing() {
         <Header />
         <Title />
         <Illustration />
+        <Features />
         <ObjectiveSection />
         <Automations />
-        <Imports />
+        {/* <Imports /> */}
         <Collaboration />
         <EarlyBird />
         <Footer />
