c21d1344d1adf30411367dfe107db3bbf7bf579e
zigmint <rupeshr053@gmail.com>
2022-12-23 20:00:08 +0530

    animate objectiv section

--- components/ObjectiveSection.js
+++ components/ObjectiveSection.js
@@ -1,35 +1,47 @@
 import { SectionHeading, SectionSubHeading } from "./Hero";
 import DesktopView from "../public/desktopView.png";
-export default function ObjectiveSection() {
+import {
+  motion,
+  useViewportScroll,
+  useTransform,
+} from "framer-motion";
+
+const ObjectiveSection = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const textX = useTransform(scrollYProgress, [0, 1], [0, -900]);
+  const imageX = useTransform(scrollYProgress, [0, 1], [1, 100]);
   return (
     <>
       <div className="text-center flex flex-col items-center mt-10 text-white">
-        <SectionHeading>
-          Work Less
-          Recruit More
-        </SectionHeading>
+        <SectionHeading>Work Less Recruit More</SectionHeading>
         <SectionSubHeading>
           FlyinOut helps you boost your Recruiting Productivity at Any Scale
         </SectionSubHeading>
       </div>
 
       <div className="my-8 relative flex flex-col sm:flex-row sm:items-stretch w-full px-4 max-w-3xl mx-auto">
-        <div
+        <motion.div
+          initial={{ x: "-100vw", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
           className="flex flex-col justify-center rounded-lg p-8 h-48 lg:h-96 w-full"
           style={{
             background:
               "linear-gradient(77.79deg, #5C70F4 -5.75%, #9347E7 111.09%)",
           }}
         >
-          <h2 className="text-primary text-base font-bold">
+          <h2 className="text-primary text-lg font-bold">
             Blazingly Fast Search
           </h2>
-          <h3 className="text-primary text-xs font-bold w-full lg:w-1/4 my-4">
+          <h3 className="text-primary text-base font-bold w-full lg:w-2/4 my-4">
             Once you have imported your candidates, jobs and companies, you can
             perform complicated searches with filters at incredible speeds.
           </h3>
-        </div>
-        <div
+        </motion.div>
+        <motion.div
+          initial={{ x: "100vw",y:'-50%', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
           className="hidden lg:block absolute rounded-lg h-80 w-80 z-10"
           style={{
             top: " 50%",
@@ -44,9 +56,12 @@ export default function ObjectiveSection() {
             border: "1px solid rgba(148, 141, 169, 0.35)",
             boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
             borderRadius: "8px",
+            x:imageX
           }}
-        ></div>
+        ></motion.div>
       </div>
     </>
   );
 }
+
+export default ObjectiveSection;
