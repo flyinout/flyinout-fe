aff3ebf5885a9697e568ac14178cfbc6e4d47c2a
zigmint <rupeshr053@gmail.com>
2022-12-24 21:28:15 +0530

    remove scaling on subheading

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export function SectionHeading({ children }) {
   return (
     <motion.h1
-      whileHover={{  scale: 1.05  }}
+      whileHover={{ scale: 1.05 }}
       className="mt-2 sm:mt-4 text-primary text-xl font-extrabold md:text-4xl"
     >
       {children}
@@ -13,10 +13,7 @@ export function SectionHeading({ children }) {
 
 export function SectionSubHeading({ children }) {
   return (
-    <motion.p
-      whileHover={{  scale: 1.1  }}
-      className="mt-2 sm:mt-4 text-secondary md:text-xl max-w-2xl shadow-sm"
-    >
+    <motion.p className="mt-2 sm:mt-4 text-secondary md:text-xl max-w-2xl shadow-sm">
       {children}
     </motion.p>
   );
