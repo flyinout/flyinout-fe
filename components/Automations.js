8a4d0f3911bf4d0466404ad9ac421a998d3844ac
zigmint <rupeshr053@gmail.com>
2023-01-03 17:53:50 +0530

    responsive dumb fix

--- components/Automations.js
+++ components/Automations.js
@@ -5,12 +5,19 @@ import { SectionHeading, SectionSubHeading } from "./Hero";
 import { faRocket } from "@fortawesome/free-solid-svg-icons";
 import Icon from "./Icon";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { useState, useEffect } from "react";
 
 export default function Automations() {
   const { scrollYProgress } = useViewportScroll();
   const assembleX = useTransform(scrollYProgress, [0, 1], [-200, 100]);
   const bringOnX = useTransform(scrollYProgress, [0, 1], [300, 0]);
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
   return (
     <>
       <motion.div className="text-center w-full sm:flex sm:flex-col sm:justify-center sm:items-center z-20">
@@ -18,12 +25,8 @@ export default function Automations() {
       </motion.div>
       <div className="my-8 flex flex-col items-center md:flex-row md:justify-center md:items-stretch md:mx-auto px-4 md:max-w-4xl">
         <motion.div
-          initial={{ x: "-100vw", opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: "100vw", opacity: 0 }}
-          transition={{ delay: 0.5, duration: 1 }}
-          className="flex flex-col rounded-lg justify-end w-full md:w-7/12 h-auto overflow-x-hidden"
-          style={{ background: "#383440", x: assembleX }}
+          className="flex flex-col rounded-lg justify-end w-full md:w-7/12 h-auto overflow-x-hidden pt-2"
+          style={{ background: "#383440", x: isMobile ? 0 : assembleX }}
         >
           <Icon icon={faRocket} size="6x" style={{ color: "#fff" }} />
           <div
@@ -48,10 +51,7 @@ export default function Automations() {
         </motion.div>
         <motion.div
           className="rounded-lg pt-8 bg-gray-700 w-full h-auto my-4 md:my-0 md:ml-4"
-          style={{ background: "#383440", x: bringOnX }}
-          initial={{ x: "100vw", opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1 }}
+          style={{ background: "#383440", x: isMobile ? 0 : bringOnX }}
         >
           <div className="px-10">
             <div className="flex items-center">
